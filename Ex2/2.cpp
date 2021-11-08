dab9b63d80ccc1b0b01a7cc0de1a07b81fce559f
rafaeldsd <rafaelsdias@ua.pt>
2021-11-08 17:50:38 +0000

    Ex2

---
+++ Ex2/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    ifstream    ifs(argv[1]);
+    ofstream    ofs(argv[2]);
+
+    ofs << ifs.rdbuf();
+    ofs.close();
+    return 0;
+} 
